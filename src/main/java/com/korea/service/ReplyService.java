5888cb48bb4e9124ead57ef5825e9209f9592afe
onomlight <pass041@nate.com>
2022-08-07 17:25:28 +0900

    et

--- src/main/java/com/korea/service/ReplyService.java
+++ src/main/java/com/korea/service/ReplyService.java
@@ -1,13 +1,19 @@
 package com.korea.service;
 
+import java.util.List;
+
 import com.korea.domain.ReplyVO;
 
 public interface ReplyService {
 	//등록
 	public int register(ReplyVO vo);
 	//조회
-	
+	public ReplyVO get(Long rno);
 	//수정
+	public int modify(ReplyVO vo);
 	//삭제
+	public int remove(Long rno);
 	//페이징
+	public List<ReplyVO> getList(Criteria cri, Long bno);
+	
 }
