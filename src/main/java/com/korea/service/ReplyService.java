855fdf58e7f14816dec76ab1fa738cd22e97d22a
onomlight <pass041@nate.com>
2022-08-07 17:22:40 +0900

    asd

--- src/main/java/com/korea/service/ReplyService.java
+++ src/main/java/com/korea/service/ReplyService.java
@@ -1,5 +1,13 @@
 package com.korea.service;
 
-public class ReplyService {
+import com.korea.domain.ReplyVO;
 
+public interface ReplyService {
+	//등록
+	public int register(ReplyVO vo);
+	//조회
+	
+	//수정
+	//삭제
+	//페이징
 }
